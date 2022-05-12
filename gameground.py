e9de6d89530b9912c307f01396c631afed93cb8f
Tyler Lawson <tylaw93@yahoo.com>
2022-05-12 13:25:12 -0500

    Added accelleration and velocity

--- gameground.py
+++ gameground.py
@@ -27,6 +27,25 @@ class player(pygame.sprite.Sprite):
         self.pos = vec((10,385))
         self.vel = vec(0,0)
         self.acc = vec(0,0)
+    def move(self):
+        self.acc = vec(0,0)
+        pressed_keys = pygame.key.get_pressed()
+        
+        if pressed_keys[K_LEFT]:
+            self.acc.x = -ACC
+        if pressed_keys[K_RIGHT]:
+            self.acc.x = ACC
+        
+        self.acc.x += self.vel.x * FRIC
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc   
+            
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+            
+        self.rect.midbottom = self.pos
       
 class platform(pygame.sprite.Sprite):
     def __init__(self):
@@ -35,15 +54,7 @@ class platform(pygame.sprite.Sprite):
         self.surf.fill((255,0,0))
         self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
         
-def move(self):
-    self.acc = vec(0,0)
-    pressed_keys = pygame.key.get_pressed()
-    
-    if pressed_keys[K_LEFT]:
-        self.acc.x = -ACC
-    if pressed_keys[K_RIGHT]:
-        self.acc.x = ACC
-        
+
 PT1 = platform()
 P1 = player()
 
@@ -52,6 +63,7 @@ all_sprites.add(PT1)
 all_sprites.add(P1)
 
 while True:
+    P1.move()
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -61,6 +73,6 @@ while True:
     
     for entity in all_sprites:
         displaysurface.blit(entity.surf, entity.rect)
-        
+    
     pygame.display.update()
     FramePerSec.tick(FPS)
